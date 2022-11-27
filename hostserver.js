548130bd32f6cdf352f4586724ff1617524651aa
Rajasekhar_Mutukuri <rajasekhar_muthukuri@capturisk.com>
2022-11-26 20:39:12 -0500

    port fixes

--- hostserver.js
+++ hostserver.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/melophile'));
+app.use(express.static(__dirname + '/dist/front-end'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/melophile/index.html'));
+res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
