5a2d30174cdc968e78b0f0fc0339054e0eadb881
Rajasekhar_Mutukuri <rajasekhar_muthukuri@capturisk.com>
2022-12-08 15:45:53 -0500

    api integration

--- src/app/musicplaylist.service.ts
+++ src/app/musicplaylist.service.ts
@@ -12,19 +12,19 @@ export class MusicplaylistService {
  constructor(private http:HttpClient) {}
 
     getPlaylist() {
-        return this.http.get('https://melophile-app.herokuapp.com/playlist/getallmusic');
+        return this.http.get('/api/playlist/getallmusic');
     }
     addSongTOplaylist(data:any){
-       return this.http.post('https://melophile-app.herokuapp.com/playlist/create',data);
+       return this.http.post('/api/playlist/create',data);
     }
     updateSongToPlaylist(id:any,data:any){
-      return this.http.put('https://melophile-app.herokuapp.com/playlist/update/'+id,data);
+      return this.http.put('/api/playlist/update/'+id,data);
     }
     deleteSongDB(id:any){
-      return this.http.delete('https://melophile-app.herokuapp.com/playlist/delete/'+id);
+      return this.http.delete('/api/playlist/delete/'+id);
     }
     createSongDB(data:any){
-      return this.http.post('https://melophile-app.herokuapp.com/playlist/create',data);
+      return this.http.post('/api/playlist/create',data);
     }
 
 
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -10,17 +10,17 @@ export class UsersService {
     constructor(private http:HttpClient) {}
 
     getUsers() {
-        return this.http.get('https://melophile-app.herokuapp.com/users');
+        return this.http.get('/api/users');
     }
     addUsers(firstName: string, lastName: string, email:  string, phoneNumber: string,)
     {
-        this.http.post('https://melophile-app.herokuapp.com/users',{ firstName, lastName, email, phoneNumber})
+        this.http.post('/api/users',{ firstName, lastName, email, phoneNumber})
         .subscribe((responseData) => {
             console.log(responseData);
         });
     }
     deleteUser(userId: string) {
-        this.http.delete("https://melophile-app.herokuapp.com/users/" + userId)
+        this.http.delete("/api/users/" + userId)
             .subscribe(() => {
                 console.log('Deleted: ' + userId);
             });
@@ -28,29 +28,29 @@ export class UsersService {
     }
     updateUser(userId: string,firstName: string, lastName: string, email:  string, phoneNumber: string)
     {
-        this.http.put("https://melophile-app.herokuapp.com/users/" +
+        this.http.put("/api/users/" +
         userId,{ firstName, lastName, email, phoneNumber })
         .subscribe(() => {
             console.log('Updated: ' + userId);
         });
     }
     getUser(userId: string) {
-        return this.http.get('https://melophile-app.herokuapp.com/users/'+ userId);
+        return this.http.get('/api/users/'+ userId);
       }
 
 
       getReviews() {
-        return this.http.get('https://melophile-app.herokuapp.com/reviews');
+        return this.http.get('/api/reviews');
     }
     addReviews(firstName: string, rate: string, review:  string)
     {
-        this.http.post('https://melophile-app.herokuapp.com/reviews',{ firstName, rate, review})
+        this.http.post('/api/reviews',{ firstName, rate, review})
         .subscribe((responseData) => {
             console.log(responseData);
         });
     }
     deleteReview(reviewId: string) {
-        this.http.delete("https://melophile-app.herokuapp.com/reviews/" + reviewId)
+        this.http.delete("/api/reviews/" + reviewId)
             .subscribe(() => {
                 console.log('Deleted: ' + reviewId);
             });
@@ -58,30 +58,30 @@ export class UsersService {
     }
     updateReview(reviewId: string,firstName: string, rate: string, review: string)
     {
-        this.http.put("https://melophile-app.herokuapp.com/reviews/" +
+        this.http.put("/api/reviews/" +
         reviewId,{ firstName, rate, review })
         .subscribe(() => {
             console.log('Updated: ' + reviewId);
         });
     }
     getReview(reviewId: string) {
-        return this.http.get('https://melophile-app.herokuapp.com/reviews/'+ reviewId);
+        return this.http.get('/api/reviews/'+ reviewId);
       }
 
 
       getSubscriptions() {
-        return this.http.get('https://melophile-app.herokuapp.com/subscription');
+        return this.http.get('/api/subscription');
     }
     addSubscription(subtype:string){
         console.log(subtype)
-        return this.http.post('https://melophile-app.herokuapp.com/subscription',{subscribed:subtype})
+        return this.http.post('/api/subscription',{subscribed:subtype})
 
     }
     deleteSubscription(collection:{}){
-        return this.http.post('https://melophile-app.herokuapp.com/deleteSubscription',{query:collection})
+        return this.http.post('/api/deleteSubscription',{query:collection})
     }
     updateSubscription(collection:{},value:string){
-        return this.http.post('https://melophile-app.herokuapp.com/updateSubscription',{query:collection,subscribed:value})
+        return this.http.post('/api/updateSubscription',{query:collection,subscribed:value})
     }
 
 
